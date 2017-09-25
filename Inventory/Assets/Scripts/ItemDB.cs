699c378f4fe2f73f9547b3586d3a8e33fbd51fd7
Takuyainc <kai.mergener@web.de>
2017-09-25 18:35:32 +0200

    htzt
    ztzt

--- Inventory/Assets/Scripts/ItemDB.cs
+++ Inventory/Assets/Scripts/ItemDB.cs
@@ -6,10 +6,6 @@ using System.Linq;
 
 public class ItemDB : MonoBehaviour {
 
-    /// <summary>
-    /// Contains all items from the item database
-    /// </summary>
-    /// 
     public  static List<Item> database;
     public int playerMoney;
     public Currency currency;
@@ -17,14 +13,14 @@ public class ItemDB : MonoBehaviour {
 
     void Awake ()            // Awake weil Datenbank mit dem ersten Frame geladen werden soll
     {
-        string path = Application.streamingAssetsPath + "/Items.json";
-        string jsonstring = File.ReadAllText(path);
-        database = JsonConvert.DeserializeObject<List<Item>>(jsonstring);       // Befüllen der Liste database mit allen Einträgen aus der JSON
+        string path = Application.streamingAssetsPath + "/Items.json";      // Application Funktion um den StreamingAssets Ordner anzugeben und den Namen der .json
+        string jsonstring = File.ReadAllText (path);                         // String jsonstring enthält den Inhalt von Items.json
+        database = JsonConvert.DeserializeObject<List<Item>> (jsonstring);   // Befüllen der Liste database mit allen Einträgen aus der JSON
     }
 
-    public void Addmoney (int money)
+    public void Addmoney (int money)    
     { 
-        playerMoney += money;
+        playerMoney += money;   // Das Geld des Spielers wird erhöht sobald Addmoney aufgerufen wird
     }
 
     /// <summary>
@@ -34,58 +30,58 @@ public class ItemDB : MonoBehaviour {
     /// <returns></returns>
     /// 
 
-    public Item GetItemByID (int ID)     //Beispielhafte Methode zum finden eines Items in meiner Datenbank anhand seiner ID
+    public Item GetItemByID (int ID)     // Beispielhafte Methode zum finden eines Items in meiner Datenbank anhand seiner ID
     {
         return database.Find (item => item.ID == ID);
     }
 
-    public Item GetbyBaseID (string BaseID, Item.TierEnum tier)      //Methode zum finden eines Items in meiner Datenbank anhand seiner BaseID und Tier
+    public Item GetbyBaseID (string BaseID, Item.TierEnum tier)      // Methode zum finden eines Items in meiner Datenbank anhand seiner BaseID und Tier
     {
         return database.Find (item => item.ItemBaseID == BaseID && item.Tier == tier);
     }
 
-    public int UpgradeMoney (Item selectedItem)
+    public int UpgradeMoney (Item selectedItem)     // Funktion um das Geld des Spielers nach einem Upgrade anzupassen
     {
         playerMoney = playerMoney - selectedItem.Value;
         currency.UpdateAmountText ();
         return playerMoney;
     }
 
-    public Item Upgradeitem (Item itemToUpgrade)    //aktuelles Item nutzen um eine Abfrage zu starten
+    public Item Upgradeitem (Item itemToUpgrade)    // Aktuelles Item nutzen um eine Abfrage zu starten
     {
        
-        if(playerMoney < itemToUpgrade.Value) { // aktuelles Geld nutzen um zu checken ob man sich ein upgrade leisten kann
+        if(playerMoney < itemToUpgrade.Value) { // Aktuelles Geld nutzen um zu checken ob man sich ein upgrade leisten kann
 
             print ("You can not upgrade further");
             return itemToUpgrade;
         }
 
-        if (playerMoney >= itemToUpgrade.Value) { // 0 macht vllt keinen sinn
+        if (playerMoney >= itemToUpgrade.Value) { 
         
-            if (itemToUpgrade.Tier == Item.TierEnum.epic) {    //abgleichen ob ein größeres Tier vorhanden ist
+            if (itemToUpgrade.Tier == Item.TierEnum.epic) {    // Abgleichen ob ein größeres Tier vorhanden ist, ansonsten macht ein Upgrade keinen Sinn
 
                 print("Highest Tier reached");
                 return itemToUpgrade;
                 }
-                                                                           //altes item im spielerinventar mit neuem tauschen
-                UpgradeMoney (itemToUpgrade);                                //Kosten für das Upgrade vom Geld abziehen
+                                                                          
+                UpgradeMoney (itemToUpgrade);                                // Kosten für das Upgrade vom Geld abziehen
                
-                return GetbyBaseID (itemToUpgrade.ItemBaseID, itemToUpgrade.Tier+1);  //Rückgabe des Items in der DB, welches durch aktuelles Item ersetzt wird
+                return GetbyBaseID (itemToUpgrade.ItemBaseID, itemToUpgrade.Tier+1);  // Rückgabe des Items in der DB, welches durch aktuelles Item ersetzt wird
             }
-        return itemToUpgrade;
+        return itemToUpgrade;       // Falls kein Fall eintritt, ursprüngliches Item wieder zurückgeben
     }
     
 
+     [System.Serializable]
+    public class Item : SerializableBase {
 
-    [System.Serializable]
-    public class Item : SerializableBase { 
-
-        protected override void OnConstruct ()
+        protected override void OnConstruct()
         {
             base.OnConstruct();
 
-            this.ID = -1;
-        }
+            this.ID = -1;       // Ich setze hier die ID des Objektes Item auf -1, da ich in einer früheren Version damit eine Sicherheitsmaßnahme getroffen habe um keine leeren
+        }                       // Items zu spawnen. Ist jetzt nichtmehr notwendig, kann aber auch nicht schaden.
+        
 
         public enum TierEnum {              // Enumeration für die Tiers (Name Tierenum)
             normal,
@@ -131,7 +127,7 @@ public class ItemDB : MonoBehaviour {
             this.itemsprite = Resources.Load<Sprite> ("Items/" + slug);
         }
 
-        public Item () : base ()
+        public Item () : base ()        // Leerer Konstruktor für Item
         {  
         }
     }
